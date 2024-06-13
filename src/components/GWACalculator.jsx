7dce8500c99436913cf39fdd7a850f29f02494de
generyand <generyan.dep@gmail.com>
2024-06-13 19:28:56 +0800

    minor changes

--- src/components/GWACalculator.jsx
+++ src/components/GWACalculator.jsx
@@ -40,7 +40,7 @@ export default function GWACalculator() {
   return (
     <div className="max-w-2xl mx-auto my-[10vh]">
       <h1 className="mx-2 mb-1 text-center title">Calculate Your GWA</h1>
-      <p className="mx-4 mb-4 text-center max-w-[40ch]:">
+      <p className="mx-4 mb-4 text-center max-w-[40ch]: text-sm lg:text-base text-gray-600">
         Welcome to our GWA Calculator, Ga! This tool is designed for students in the
         University of Mindanao to calculate their General Weighted Average
         (GWA). Just input your grades, and let the calculator do the rest! 🤗
